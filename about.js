cf61caaf9a7bd9f293023cf7ae8facbea9e4eb83
fasoyin kolawole <kodavtech@gmail.com>
2026-03-04 16:31:20 +0100

    finishing touches of the service page

---
+++ about.js
@@ -0,0 +1,15 @@
+// Scroll Reveal Animation
+
+window.addEventListener("scroll", function () {
+    const reveals = document.querySelectorAll(".reveal");
+
+    reveals.forEach((element) => {
+        const windowHeight = window.innerHeight;
+        const elementTop = element.getBoundingClientRect().top;
+        const revealPoint = 100;
+
+        if (elementTop < windowHeight - revealPoint) {
+            element.classList.add("active");
+        }
+    });
+});
